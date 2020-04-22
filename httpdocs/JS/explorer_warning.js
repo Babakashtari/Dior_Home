39d9561a092492b82b800096a9525e4989329237
babakashtari <ashtaribabak@rocketmail.com>
2020-04-22 16:08:01 +0430

    some modifications on sublimation page

--- httpdocs/JS/explorer_warning.js
+++ httpdocs/JS/explorer_warning.js
@@ -5,15 +5,16 @@ const footer_img = document.querySelector("footer>img");
 const browser_array = ["MSIE", "Trident", "Edge"];
 const user_agent = navigator.userAgent;
 for (let b = 0; b < browser_array.length; b++) {
-    if (user_agent.indexOf(browser_array[b]) > -1) {
-        warning.classList.remove("displayNone");
-        if (site_header.classList.contains("sticky-top")) {
-            setInterval(() => {
-                site_header.classList.remove("sticky-top");
-                site_header.style.zIndex = -1;
-                footer_img.style.zIndex = 0;
-            }, 1);
-        }
-        break;
+  if (user_agent.indexOf(browser_array[b]) > -1) {
+    console.log("for rest");
+    warning.classList.remove("displayNone");
+    if (site_header.classList.contains("sticky-top")) {
+      setInterval(() => {
+        site_header.classList.remove("sticky-top");
+        site_header.style.zIndex = -1;
+        footer_img.style.zIndex = 0;
+      }, 1);
     }
+    break;
+  }
 }
