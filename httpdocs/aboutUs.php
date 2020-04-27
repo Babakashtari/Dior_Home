c6c8d6e95e779787cf2b73d2ab7024ace48f1f47
babakashtari <ashtaribabak@rocketmail.com>
2020-04-27 13:08:13 +0430

    initialized an aboutUs.php page

---
+++ httpdocs/aboutUs.php
@@ -0,0 +1,72 @@
+<?php require "php/code_functions.php" ?>
+<?php require 'php/explorer_warning.php' ?>
+
+<!-- <?php session_start(); ?> -->
+<!DOCTYPE html lang="fa">
+<html lang="fa">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="content-security-policy" content="default-src 'none'; 
+        style-src 'self' 'unsafe-inline'; 
+        script-src 'self' 'unsafe-inline';
+        img-src 'self';
+        font-src 'self';
+        frame-src https://www.google.com;
+        "  >
+    <meta name="description"    content="سايت رسمی ‍پیشگامان ‍پودينه آتا - درباره ما" />
+    <meta name="author" content="Babak Ashtari" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="images/fav_icon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/fav_icon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/fav_icon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/fav_icon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/fav_icon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/fav_icon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/fav_icon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/fav_icon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="images/fav_icon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/fav_icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="images/fav_icon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/fav_icon/favicon-16x16.png">
+    <link rel="manifest" href="images/fav_icon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="images/fav_icon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="CSS/all.min.css">
+    <link rel="stylesheet" href="CSS/bootstrap.min.css.map">
+    <link rel="stylesheet" href="CSS/bootstrap.min.css"/>
+    <link rel="stylesheet" href="CSS/Normalizer.css">
+    <link rel="stylesheet" href="Css/fonts.css">
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/footer.css">
+    <link rel="stylesheet" href="CSS/explorer_warning.css">
+    <link rel="stylesheet" href="CSS/aboutUs.css">
+    <title>پيشگامان پودينه - درباره ما</title>
+</head>
+<body>
+    <?php head(); ?>
+    <main>
+        <section>
+            <div class="col-10 mx-auto bg-light text-dark text-center border border-primary my-4 iranSans">
+                <h4 class="p-4">درباره پيشگامان پودينه آتا</h4>
+                <p class="text-justify">شركت پيشگامان پودينه آتا مفتخر است كه با بيش از 30 سال تجربه در حوزه توليد و فروش عمده پارچه خدمت رسان شركت ها و تولیدی های عمده فروشی در سطح کل کشور است.</p>
+            </div>
+        </section>
+        <?php show_warning(); ?>
+    </main>
+    <?php footer_generator();?>
+    <?php canvas_generator(); ?>
+    <script src="JS/explorer_warning.js"></script>
+    <script src="JS/canvas.js"></script>
+    <script src="JS/jquery.min.js"></script>
+    <script src="JS/bootstrap.min.js"></script>
+    <script src="JS/header.js"></script>
+    <script src="JS/aboutUs.js"></script>
+    <script src="JS/footer.js"></script>
+</body>
+</html>
