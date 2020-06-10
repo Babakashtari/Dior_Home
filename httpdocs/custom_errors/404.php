5961a7aa9e39ddb6a17ba5a403ef90a9f7084817
babakashtari <ashtaribabak@rocketmail.com>
2020-06-11 00:44:30 +0430

    started a 404 custom error page

---
+++ httpdocs/custom_errors/404.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html lang="fa">
+<html lang="fa">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta
+            http-equiv="content-security-policy"
+            content="default-src 'none'; 
+        style-src 'self' 'unsafe-inline'; 
+        script-src 'self' 'unsafe-inline' https://www.google-analytics.com;
+        img-src 'self' https://www.w3.org https://www.google.com.ua https://www.google.com https://www.google-analytics.com https://www.googletagmanager.com https://stats.g.doubleclick.net;
+        font-src 'self';
+        frame-src https://www.google.com;
+        "
+        />
+        <meta name="author" content="Babak Ashtari" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="images/fav_icon/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="images/fav_icon/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="images/fav_icon/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="images/fav_icon/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="images/fav_icon/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="images/fav_icon/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="images/fav_icon/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="images/fav_icon/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="images/fav_icon/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="images/fav_icon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="images/fav_icon/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="images/fav_icon/favicon-16x16.png" />
+        <link rel="manifest" href="images/fav_icon/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="images/fav_icon/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <!-- font awesome -->
+        <link rel="stylesheet" href="CSS/all.min.css" />
+        <link rel="stylesheet" href="CSS/bootstrap.min.css.map" />
+        <link rel="stylesheet" href="CSS/bootstrap.min.css" />
+        <link rel="stylesheet" href="CSS/Normalizer.css" />
+        <link rel="stylesheet" href="CSS/fonts.css" />
+        <link rel="stylesheet" href="CSS/404.css">
+        <title>پيشگامان پودينه - صفحه اصلی</title>
+    </head>
+    <body>
+        <main>
+            <div class="logo-section">
+                <img src="../images/Dior_logo.jpg" alt="لوگوی پیشگامان پودینه آتا" width="100vw" height="auto" />
+            </div>
+            <section class="error-section">
+                <h3>خطای 404! </h3>
+                <p>صفحه مورد نظر یافت نشد. لطفا از لینک های زیر استفاده نمایید:</p>
+            </section>
+        </main>
+        <script src="JS/explorer_warning.js"></script>
+        <script src="JS/jquery.min.js"></script>
+        <script src="JS/bootstrap.min.js"></script>
+    </body>
+</html>
